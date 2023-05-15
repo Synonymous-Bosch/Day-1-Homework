1151c2704b0d911a1d2e04afa4673305b045dc31
Steven <stevenjohnmorgan@gmail.com>
2023-05-15 16:25:07 +0100

    Added notes to code for readability

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,8 +1,12 @@
 #! Python3
 
+# Setting variables
+
 name = 'Steven'
 age = 39
 favourite_cheese = 'cheddar'
+
+# Defining a function to ask the user their name, age, and favourite cheese.
 
 def someone_loves():
      print("Hi! What's your name?")
@@ -11,6 +15,7 @@ def someone_loves():
      age = input()
      print("That's genuinely fantastic. Now the rub. What is your favourite cheese?")
      cheese = input()
+     # Use of operators to concatenate inputs into sassy response
      they_love = "Let me see if I've got this straight. " + name + ', who is ' + str(age) + ' years old, loves ' + favourite_cheese + '? What a poser.'
      print(they_love)
 
